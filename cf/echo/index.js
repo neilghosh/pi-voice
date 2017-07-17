6bfb57d5a467854414cbed4c0ed8a66e5f901c32
Neil Ghosh <neil.ghosh@gmail.com>
2017-07-17 20:07:38 +0530

    Added more info quoteType

--- cf/echo/index.js
+++ cf/echo/index.js
@@ -1,34 +1,34 @@
 const request = require('request');
 const MAX_COMPANIES_IN_RESPONSE = 2;
 
-function getCompanies(name, res, handleCompanies, handlePrice) {
+function getCompanies(name, quoteType, res, handleCompanies, handlePrice) {
     request.get('https://trade-junky.appspot.com/companysearch?name=' + name, function(error, response, body) {
         console.log('error:', error); // Print the error if one occurred 
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
         console.log('body:', body); //Prints the response of the request. 
-        handleCompanies(res, body, handlePrice)
+        handleCompanies(quoteType,res, body, handlePrice)
     });
 }
 
-function getPrice(tickerSym, res, handlePrice) {
+function getPrice(tickerSym, quoteType, res, handlePrice) {
     request.get('https://trade-junky.appspot.com/getquote?name=' + tickerSym, function(error, response, body) {
         console.log('error:', error); // Print the error if one occurred 
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
         console.log('body:', body); //Prints the response of the request. 
-        handlePrice(res, body)
+        handlePrice(quoteType, res, body)
     });
 }
 
-handlePrice = function(res, responseOutStr) {
+handlePrice = function(quoteType, res, responseOutStr) {
     responseOut = JSON.parse(responseOutStr);
     res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
     res.send(JSON.stringify({
-        "speech": "Last price was " + responseOut.lastPrice,
+        "speech": "Last price was " + responseOut[quoteType],
         "displayText": responseOut.lastPrice
     }));
 }
 
-handleCompanies = function(res, responseOutStr, handlePrice) {
+handleCompanies = function(quoteType,res, responseOutStr, handlePrice) {
     responseOut = JSON.parse(responseOutStr);
     var response = "";
     var companyCount = Object.keys(responseOut).length;
@@ -58,7 +58,7 @@ handleCompanies = function(res, responseOutStr, handlePrice) {
 
     } else {
         var tickerSym = Object.keys(responseOut)[0];
-        getPrice(tickerSym, res, handlePrice);
+        getPrice(tickerSym, quoteType, res, handlePrice);
     }
 }
 
@@ -73,7 +73,8 @@ exports.helloWorld = function helloWorld(req, res) {
     var intent = req.body.result.metadata.intentName
     if (intent == "Tell-Stocks" || intent == "Tell-Stocks - Tell Price") {
         var companyname = req.body.result.parameters.companyname;
-        getCompanies(companyname, res, handleCompanies, handlePrice)
+        var quoteType = req.body.result.parameters.quotetype
+        getCompanies(companyname, quoteType, res, handleCompanies, handlePrice)
     } else {
         var p1 = req.body.result.parameters.insights;
         console.log(p1);
